d3893b04fe65787913c3d6e22f56f1cee9852433
Jussivan-IF <115298615+Jussivan-IF@users.noreply.github.com>
2025-03-11 15:26:17 -0300

    Update gameRoutes.js

--- routes/gameRoutes.js
+++ routes/gameRoutes.js
@@ -7,6 +7,7 @@ router.get('/games', listGames);
 router.get('/games/:id', getGameById);
 router.post('/games', createGame);
 router.put('/games/:id', updateGame);
+
 router.delete('/games/:id', deleteGame);
 
 export default router;
